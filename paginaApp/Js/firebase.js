7faaff777a46946da329fe6f6665c1c11e870133
Vractos <53495782+Vractos@users.noreply.github.com>
2020-01-01 21:40:30 -0300

    Update

---
+++ paginaApp/Js/firebase.js
@@ -0,0 +1,15 @@
+(function() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAld8pYy0EQHOCb7dTPrgBVPgtdHzaJKXA",
+        authDomain: "uuaall.firebaseapp.com",
+        databaseURL: "https://uuaall.firebaseio.com",
+        projectId: "uuaall",
+        storageBucket: "uuaall.appspot.com",
+        messagingSenderId: "242937701085",
+        appId: "1:242937701085:web:a3c5553a7be86a619e3134",
+        measurementId: "G-8Y838DQP19"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})()
