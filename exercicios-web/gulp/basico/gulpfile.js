6f0a12e7cbf45e1f20d45ce55774f2b6462771a3
Vractos <53495782+Vractos@users.noreply.github.com>
2020-05-28 21:01:15 -0300

    update

---
+++ exercicios-web/gulp/basico/gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
+
+const antes1 = cb => {
+    console.log('Tarefa antes 1!')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa antes 2!')
+    return cb()
+}
+
+function copiar(cb) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')     //NOTE: Essa expressão esta pegando qualquer arquivo dentro da 'pastaA' e de suas subpastas, que seja do tipo '.txt'
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+)
