aea0e06ef6f3f2a25e5dff2fe36d30324a62a405
Vractos <53495782+Vractos@users.noreply.github.com>
2020-06-03 12:21:53 -0300

    Cap. React

--- exercicios-web/react/exercicios/src/index.js
+++ exercicios-web/react/exercicios/src/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
-    <div>
-        <Pai nome="Paulo" sobrenome="Silva"></Pai>
-    </div>
+    <Pai nome="Paulo" sobrenome="Silva">
+        <Filho nome="Pedro"></Filho>
+        <Filho nome="Paulo"></Filho>
+        <Filho nome="Carla"></Filho>
+    </Pai>
 , document.getElementById('root'))
